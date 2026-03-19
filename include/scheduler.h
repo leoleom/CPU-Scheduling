b77545ecf1100ca58101fc379073da3c7c742686
rykonnen <lqblancaflor@up.edu.ph>
2026-03-20 04:09:21 +0800

    refactor: improve code formatting and structure in scheduler.h

--- include/scheduler.h
+++ include/scheduler.h
@@ -2,15 +2,17 @@
 #define SCHEDULER_H
 
 #include "process.h"
+#include "heap.h"
 #include <stdlib.h>
 
-
-typedef struct Node {
+typedef struct Node
+{
     Process *process;
     struct Node *next;
 } Node;
 
-typedef struct {
+typedef struct
+{
     Node *head;
     Node *tail;
     int size;
@@ -18,34 +20,38 @@ typedef struct {
 
 #include "mlfq.h"
 
-typedef struct {
-    Process *processes;     // Array of all processes
-    int num_processes;      // Number of processes
-    int current_time;       // Current simulation time
+typedef struct
+{
+    Process *processes; // Array of all processes
+    int num_processes;  // Number of processes
+    int current_time;   // Current simulation time
 
     Queue ready_queue;
-    Process *current_process;  // current running process
-    char *gantt_chart;       
-    int gantt_size;          
+    Process *current_process; // current running process
+    char *gantt_chart;
+    int gantt_size;
 
-    //store metrics
+    // store metrics
 } SchedulerState;
 
-typedef enum {
+typedef enum
+{
     EVENT_ARRIVAL,
     EVENT_COMPLETION,
     EVENT_QUANTUM_EXPIRE,
     EVENT_PRIORITY_BOOST
 } EventType;
 
-typedef struct Event {
+typedef struct Event
+{
     int time;
     EventType type;
     Process *process;
     struct Event *next;
 } Event;
 
-typedef enum {
+typedef enum
+{
     FCFS,
     SJF,
     STCF,
@@ -56,12 +62,21 @@ typedef enum {
 void init_scheduler(SchedulerState *state);
 
 void enqueue(Queue *queue, Process *proc);
-Node* dequeue(Queue *queue);
+Node *dequeue(Queue *queue);
+
+// fcfs and rr
+void handle_arrivals_queue(SchedulerState *state, int current_time);
+// sjf scheduler
+void handle_arrivals_sjf(SchedulerState *state, MinHeap *heap, int current_time);
+// stcf scheduler
+void handle_arrivals_stcf(SchedulerState *state, MinHeap *heap, int current_time);
+// mlfq scheduler
+void handle_arrivals_mlfq(SchedulerState *state, MLFQQueue *queues, int current_time);
 
 // Return 0 on success, -1 on error (command line etiquette)
 int schedule_fcfs(SchedulerState *state);
-int schedule_sjf(SchedulerState *state);
-int schedule_stcf(SchedulerState *state);
+int schedule_sjf(SchedulerState *state, MinHeap *heap);
+int schedule_stcf(SchedulerState *state, MinHeap *heap);
 int schedule_rr(SchedulerState *state, int quantum);
 int schedule_mlfq(SchedulerState *state, MLFQConfig *config);
 
