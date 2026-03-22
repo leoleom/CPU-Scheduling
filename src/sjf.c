c084398246fe73ec230808a07b639e6fbe9c32b4
rykonnen <lqblancaflor@up.edu.ph>
2026-03-23 05:03:30 +0800

    refactor: update sjf to implement event-driven scheduling

--- src/sjf.c
+++ src/sjf.c
@@ -9,46 +9,25 @@ int schedule_sjf(SchedulerState *state, MinHeap *heap) {
 
     if (!state || state->num_processes == 0)
         return -1;
-
-    int completed = 0;
-    int time = 0;
-    int gantt_index = 0;
     
+    if (state->current_process != NULL)
+        return 0;
+    
+    // do nothing if heap is empty
+    if (heap->size == 0)
+        return 0;
 
-    while (completed < state->num_processes) {
+    // pick shortest job from heap
+    Process *shortest = heap_extract_min(heap, cmp_sjf);
+    state->current_process = shortest;
 
-        // ARRIVALS
-        handle_arrivals_sjf(state, heap, time);
+    // record start time
+    if (shortest->start_time == -1)
+        shortest->start_time = state->current_time;
 
-        // pick next process if idle
-        if (!state->current_process && heap->size > 0) {
+    // schedule completion event
+    int finish_time = state->current_time + shortest->remaining_time;
+    schedule_event(state, shortest, EVENT_COMPLETION, finish_time);
 
-            state->current_process =
-                heap_extract_min(heap, cmp_sjf);
-
-            if (state->current_process->start_time == -1)
-                state->current_process->start_time = time;
-        }
-
-        // execute current process
-        if (state->current_process) {
-
-            char pid = state->current_process->pid[0];
-            gantt_add(gantt_index, pid);
-            gantt_index++;
-        
-            state->current_process->remaining_time--;
-
-            if (state->current_process->remaining_time == 0) {
-                state->current_process->finish_time = time + 1;
-                completed++;
-                state->current_process = NULL;
-            }
-        }
-
-        time++;
-    }
-
-    gantt_print(time);
     return 0;
 }
